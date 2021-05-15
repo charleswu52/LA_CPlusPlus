f0375f83424112ce37cd842e2578eab61d982a62
charleswu52 <charleswu5201@gmail.com>
2021-05-15 20:49:09 +0800

    Initial commit

---
+++ Trie.h
@@ -0,0 +1,26 @@
+#ifndef __TRIE_H_
+#define __TRIE_H_
+#include <string>
+using namespace std;
+
+const int ALPHABET_SIZE = 26;
+typedef struct TrieNode
+{
+    struct TrieNode* children[ALPHABET_SIZE];
+    bool isEndWord;
+    char key;
+    std::string  value;
+}TrieNode;
+TrieNode *getNode(char key, std::string value);
+
+class Trie
+{
+public:
+    Trie();
+    TrieNode* rootNode;
+    void Insert(string key);
+    bool Search(string key);
+};
+
+
+#endif
---
+++ entry.h
@@ -0,0 +1,14 @@
+//
+// Created by Charles_PC on 2021/5/15.
+//
+
+#ifndef LA_C___ENTRY_H
+#define LA_C___ENTRY_H
+
+
+class entry {
+
+};
+
+
+#endif //LA_C___ENTRY_H

d69d27a9774e82791323e9a2b6d228be78bd3757
virgilwjj <virgilwjj@gmail.com>
2021-05-16 19:10:38 +0800

    del file

--- tests/entry.cpp
+++ tests/entry.cpp
@@ -3,49 +3,58 @@
 #include "levenshtein_automaton/LevenshteinDFA.h"
 
 #include <string>
-#include <stdio.h>
+#include <vector>
 #include <list>
 #include <iostream>
 #include <fstream>
 #include <ctime>
 
+static const std::string documents_path{"../resources/wordList.txt"};
+static const std::string queries_path{"../resources/wordsToTest.txt"};
+
+std::vector<std::string>
+read_all_lines(const std::string &path)
+{
+    std::vector<std::string> result;
+    std::ifstream ifs{path, std::ifstream::in};
+    std::string line;
+    while (getline(ifs, line))
+        result.push_back(std::move(line));
+    ifs.close();
+    return result;
+}
+
 int la::START = 0;
 int main(int argc, char *argv[])
 {
-    std::ifstream file;
+    std::vector<std::string> documents{read_all_lines(documents_path)};
+    std::vector<std::string> queries{read_all_lines(queries_path)};
+
     la::Trie trie;
     clock_t timer;
     double duration = 0;
     double totalTime = 0;
     int maxDist = 2;
-    char line[256];
 
     la::LevenshteinNFA *nfa;
     la::LevenshteinDFA *dfa;
     trie.rootNode = la::getNode(' ', "");
-    file.open("../resources/wordList.txt");
     std::cout << "Constructing Trie..." << std::endl;
     timer = clock();
     /*Load in all resources*/
-    while (file.good())
-    {
-        file.getline(line, 256);
-        trie.Insert(line);
-    }
-    file.close();
+    for (auto &&document : documents)
+        trie.Insert(document);
     duration = (clock() - timer) / (double)CLOCKS_PER_SEC;
     std::cout << "Constructing Trie Complete! Time: " << duration << " Seconds" << std::endl;
     std::cout << "Levenshteins Distance: " << maxDist << std::endl;
-    file.open("../resources/wordsToTest.txt");
     //Search
-    while (file.good())
+    for (auto &&query : queries)
     {
-        file.getline(line, 256);
         std::cout << "===================================================================" << std::endl;
-        std::cout << "Constructing Levenshteins Automata for word: " << line << std::endl;
+        std::cout << "Constructing Levenshteins Automata for word: " << query << std::endl;
         timer = clock();
 
-        nfa = la::LevenshteinNFA::ConstructNFA(line, maxDist);
+        nfa = la::LevenshteinNFA::ConstructNFA(query, maxDist);
         dfa = la::LevenshteinDFA::SubsetConstruct(nfa);
         std::list<std::string> output;
         std::cout << "Searching..." << std::endl;
@@ -59,7 +68,6 @@ int main(int argc, char *argv[])
         std::cout << "Number of hits found: " << output.size() << std::endl;
     }
     std::cout << "Total time: " << totalTime << " Seconds.." << std::endl;
-    file.close();
     //Clean Up
     delete nfa;
     delete dfa;
